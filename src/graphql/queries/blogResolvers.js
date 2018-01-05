cd4d1090b6315136b26f2e7cd7c7bb3b0dd5c3f3
burczu <b.dybowski@gmail.com>
2018-01-05 16:20:00 +0100

    getBlogs call fixed

--- src/graphql/queries/blogResolvers.js
+++ src/graphql/queries/blogResolvers.js
@@ -2,7 +2,7 @@ import { Blogs } from '../../models';
 
 export const blogResolvers = {
   getAll: async (root, { page }) => {
-    const blogs = await Blogs.getBlogs(page);
+    const blogs = await Blogs.getBlogs(page - 1);
     return blogs;
   }
 };
